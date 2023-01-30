c1181bdd7228570aa2e67a9c31f8134ff1c5448f
lcaffarelli2022 <lcaffarelli2022@16SE44-H2J3P02.ad.campus-eni.fr>
2023-01-30 15:20:14 +0100

    Creation manager et BusinessException

---
+++ src/main/java/fr/eni/eneDW2M147/businessException/BusinessException.java
@@ -0,0 +1,26 @@
+package fr.eni.eneDW2M147.businessException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<String> listeMessage;
+
+	public BusinessException() {
+		this.listeMessage = new ArrayList<>();
+	}
+
+	public List<String> getListeMessage() {
+		return listeMessage;
+	}
+
+	public void addMessage(String message) {
+		this.listeMessage.add(message);
+	}
+}
--- src/main/java/fr/eni/eneDW2M147/dal/EnchereDAO.java
+++ src/main/java/fr/eni/eneDW2M147/dal/EnchereDAO.java
@@ -1,4 +1,4 @@
-package fr.eni.eneDW2M147.Dal;
+package fr.eni.eneDW2M147.dal;
 
 public class EnchereDAO {
 	
