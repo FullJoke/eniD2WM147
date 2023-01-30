37218509ca7043b5693a78f32cd5e80936dfee4c
lcaffarelli2022 <lcaffarelli2022@16SE44-H2J3P02.ad.campus-eni.fr>
2023-01-30 17:07:38 +0100

    Ajout d'une methode dans le manager

--- src/main/java/fr/eni/eniD2WM147/bll/EnchereManager.java
+++ src/main/java/fr/eni/eniD2WM147/bll/EnchereManager.java
@@ -1,8 +1,19 @@
 package fr.eni.eniD2WM147.bll;
 
+import fr.eni.eneDW2M147.businessException.BusinessException;
 import fr.eni.eneDW2M147.dal.EnchereDAO;
+import fr.eni.eniD2WM147.bo.Utilisateur;
 
 public class EnchereManager {
 	private EnchereDAO enchereDAO;
-
+	
+	public Utilisateur getUserByEmailAndPasswords(String email,String mdp) throws BusinessException {
+		try {
+			return enchereDAO.getUserByEmailAndPassword(email, mdp);
+		}catch (BusinessException e ) {
+			throw new BusinessException();
+		}
+		
+		
+	}
 }
