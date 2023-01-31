115cdd350ae5dc15858843ace323fa63a8985854
smbongui2022 <smbongui2022@16SE44-5TH3P02.ad.campus-eni.fr>
2023-01-31 16:34:35 +0100

    mise ajour

--- src/main/java/fr/eni/eneDW2M147/dal/EnchereDAO.java
+++ src/main/java/fr/eni/eneDW2M147/dal/EnchereDAO.java
@@ -1,6 +1,9 @@
 package fr.eni.eneDW2M147.dal;
 
+import java.util.List;
+
 import fr.eni.eneDW2M147.businessException.BusinessException;
+import fr.eni.eniD2WM147.bo.Categorie;
 import fr.eni.eniD2WM147.bo.Utilisateur;
 
 public interface EnchereDAO {
@@ -10,7 +13,7 @@ public interface EnchereDAO {
 	public Utilisateur insertUtilisateur(String pseudo, String nom, String prenom, String email, String telephone,
 			String rue, String code_postal, String ville, int credit, boolean administrateur) throws BusinessException;
 
-	
+	public List<Categorie> selectCategorie() throws BusinessException;
 
 
 }
