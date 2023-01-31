365aef0d098be7d4e6ecbfdf7808d8d2df784eb5
lcaffarelli2022 <lcaffarelli2022@16SE44-H2J3P02.ad.campus-eni.fr>
2023-01-31 11:00:29 +0100

    continuité des ajouts pour les erreurs

--- src/main/java/fr/eni/eniD2WM147/bll/EnchereManager.java
+++ src/main/java/fr/eni/eniD2WM147/bll/EnchereManager.java
@@ -4,7 +4,6 @@ package fr.eni.eniD2WM147.bll;
 import fr.eni.eneDW2M147.businessException.BusinessException;
 import fr.eni.eneDW2M147.dal.EnchereDAO;
 import fr.eni.eneDW2M147.dal.EnchereDAOFactory;
-import fr.eni.eneDW2M147.dal.EnchereDaoJdbcImpl;
 import fr.eni.eniD2WM147.bo.Utilisateur;
 
 
@@ -18,18 +17,26 @@ public class EnchereManager {
 
 	public Utilisateur getUserByEmailAndPassword(String email, String mdp) throws BusinessException {
 		BusinessException bE = new BusinessException();
-		if (email.equals(null)) {
-			bE.addMessage("L'email n'est pas valide");
-		}
-		if (mdp.equals(null)) {
-			bE.addMessage("Le mot de passe n'est pas valide");
-		}
+		validerEmail(email, bE);
+		validerMDP(mdp, bE);
 		
-		if(!bE.getListeMessage().isEmpty()) {
-			throw bE;
+		if(bE.getListeMessage().isEmpty()) {
+			
 		}
 		return enchereDAO.getUserByEmailAndPassword(email, mdp);
 
+	}
+	
+	private void validerEmail(String email, BusinessException businessException ) {
+		if(email==null ) {
+			businessException.addMessage("L'email n'est pas valide");
+		}
+	}
+	
+	private void validerMDP(String mdp, BusinessException businessException) {
+		if(mdp==null) {
+			businessException.addMessage("Le mot de passe n'est pas valide");
+		}
 	}
 
 }
